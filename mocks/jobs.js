1cab9a377f5e9ce46054e3bcabb3f3d8c8a3103f
BigBrosh <maksuhin@gmail.com>
2020-06-04 12:13:43 +0300

    - added jobs.

--- mocks/jobs.js
+++ mocks/jobs.js
@@ -19,5 +19,26 @@ export const mockedJobs = [
     skills: ['html', 'css', 'js', 'react', 'mobx', 'mongodb', 'nodejs', 'express'],
     budget: 40000,
     workload: '40+ hrs/week'
+  },
+  {
+    id: '4',
+    title: 'Shop - QA',
+    skills: ['jest', 'enzyme'],
+    budget: 30000,
+    workload: '40+ hrs/week'
+  },
+  {
+    id: '5',
+    title: 'Shop - Scrum master',
+    skills: ['scrum', 'jira'],
+    budget: 20000,
+    workload: '40+ hrs/week'
+  },
+  {
+    id: '6',
+    title: 'Shop - Product owner',
+    skills: ['scrum', 'jira'],
+    budget: 40000,
+    workload: '40+ hrs/week'
   }
 ];
